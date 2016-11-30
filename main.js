44d7ae2ab718b34cbc55b5c49494796126166491
Melissa Young <melissay94@yahoo.com>
2016-11-30 12:47:36 -0500

    Started to work on results panel looks

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function openSide() {
+    document.getElementById("slideout").style.width = "400px";
+}
+
+function closeSide() {
+    document.getElementById("slideout").style.width = "0";
+}
