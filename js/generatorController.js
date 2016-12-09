1ecc8d767ca83c07a70d3bbb4fa3e7f410b4ece0
Melissa A. Young (RIT Student) <may4028@ad.rit.edu>
2016-12-09 00:01:10 -0500

    Shit to be fixed

--- js/generatorController.js
+++ js/generatorController.js
@@ -6,31 +6,42 @@ mainApp.controller('generatorCtrl', ["$scope", function($scope){
     $scope.getData = function() {
         // Build up search query. For now, just do it with a random number and not the specifics 
         var my_data;
-        var url = "http://www.recipepuppy.com/api/?";
+        var url = "https://crossorigin.me/http://www.recipepuppy.com/api/";
         var randPageNum = Math.floor(Math.random() * 100 + 1);
 
-        url += "p=" + randPageNum.toString();
+        //url += "p=" + randPageNum.toString();
         console.log("results: " + url);
 
         // Call the service and download the results
-        $.getJSON('http://www.recipepuppy.com/api/', function(data){
+        /**var temp = $.getJSON("https://crossorigin.me/http://www.recipepuppy.com/api/?p=78", function(data){
             my_data = dataFetched(data);
-            console.log("Inside: " , my_data);
-            
+            localStorage.test = JSON.stringify(my_data);
+            console.log(my_data, " ", url)
+        });**/
+        console.log("my page: ", randPageNum);
+        $.ajax({
+            dataType: "json",
+            url: url,
+            data: {"i": "onions, garlic","p": randPageNum.toString()},
+            success: dataFetched
         });
-        console.log(my_data);
-        $scope.results.push({title: "Example 2", ingredients: "example 2"});
+        
+        my_data = JSON.parse(localStorage.test);
+        console.log("wtf ", my_data)
+        $scope.results = my_data.results;
+        console.log("trying something: ", $scope.results);
     }
     
     // Fetched data let's look
     function dataFetched(obj){
-
+        
         if (obj.results.length == 0) {
-            var status = "No recipes found";
+            console.log("No recipes found");
         }
         
         else {
-            return obj.results;
+            localStorage.test = JSON.stringify(obj);
+            console.log('Data: ' + localStorage.test);
         }
     }
 }]);
