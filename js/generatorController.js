f1a0e4b4a5d9f545b165c089a1e513bd314b4083
Melissa <melissay94@yahoo.com>
2016-12-10 16:41:43 -0500

    Just some small changes to follow grading guidelines

--- js/generatorController.js
+++ js/generatorController.js
@@ -24,27 +24,46 @@ mainApp.controller('generatorCtrl', ["$scope", "$http", function($scope, $http){
             data: {"p": randPageNum.toString()},
             success: dataFetched
         });
-        
-        my_data = JSON.parse(localStorage.test);
-        $scope.results = my_data.results;
     }
     
     // Fetched data let's look
     function dataFetched(obj){
         
         if (obj.results.length == 0) {
-            console.log("No recipes found");
+            $scope.results = [{ title: "No recipes found at this time", ingredients: "We seem to be having issues finding our cookbook! Feel free to contact us if this continues.", img: "default-img.png"}];
         }
         
         else {
-            localStorage.test = JSON.stringify(obj);
+            localStorage.test = JSON.stringify(obj);        
+            my_data = JSON.parse(localStorage.test);
+
+            if (my_data.length < 1) {
+                $scope.results = [
+                    { title: "Mexican Fiesta Bowl", ingredients: "Chicken Breasts, White Rice, Garlic, Black Beans, Limes, Bell Peppers, Red Onions, Diced Tomatoes, Corn"},
+                    { title: "American StirFry", ingredients: "Chicken Breasts or Pork Chops, White Rice, StirFry Sauce, White Onions, Bell Peppers, Broccoli, Carrots, Water Chestnuts, Baby Corn"},
+                    { title: "Brazilian Okra Stew", ingredients: "Chunk Steak, White Rice, Diced Tomatoes, Okra, Garlic, White Onions"}, 
+                    { title: "Mamazinha's Cheese Bread", ingredients: "Cheese bread flour, chedder jack cheese, bell peppers, ham, eggs, water"}
+                ];
+            } 
+            else {
+                $scope.results = my_data.results;
+            }
         }
     }
+
 
     // Organizes selected recipes
     $scope.datePick = function(day, recipe) {
 
         var ingredientsList = recipe.ingredients.split(',');
+
+        // Adds those ingredients to the shopping list
+        for (var j = 0; j < ingredientsList.length; j++){
+            var itemIndex = $scope.shoppingList.indexOf(ingredientsList[j])
+
+            if ( itemIndex == -1)
+                $scope.shoppingList.push(ingredientsList[j]);
+        }
 
         // Add the recipe to the schedule
         for (var i = 0; i < $scope.currentList.length; i++) {
@@ -54,19 +73,12 @@ mainApp.controller('generatorCtrl', ["$scope", "$http", function($scope, $http){
                     getNutrionFacts(element);
                     nutriTotal += nutriObj.nf_calories;
                 });
-                recipe.calories = nutriTotal;
+                recipe.calories = Math.round(nutriTotal);
                 $scope.currentList[i].recipes.push(recipe);
                 break;
             }
         }
 
-        // Adds those ingredients to the shopping list
-        for (var j = 0; j < ingredientsList.length; j++){
-            var itemIndex = $scope.shoppingList.indexOf(ingredientsList[j])
-
-            if ( itemIndex == -1)
-                $scope.shoppingList.push(ingredientsList[j]);
-        }
     }
 
     function getNutrionFacts(grocery) {
--- js/main.js
+++ js/main.js
@@ -1,31 +1,42 @@
 "use strict";
+
+// The Angular app does not like the IIFE, so he's gonna stay out here.
 var mainApp = angular.module("mainApp", []);
 
-// Functions that control the sliding out summary box
-function openSide() {
-    document.getElementById("slideout").style.width = "400px";
-}
+// Wrap it in a IIFE
+(function() {
 
-function closeSide() {
-    document.getElementById("slideout").style.width = "0";
-}
-
-function changeView() {
-	console.log("Hello");
-	var element_1 = document.getElementById("recipeSchedule");
-	var element_2 = document.getElementById("shoppingList");
-
-	if (element_1.style.display == "none"){
-		element_1.style.display = "block";
-		element_2.style.display = "none";
+	// Assigns all the buttons not making queries their functions
+	window.onload = function() {
+		document.querySelector('#sliderButton').onclick=openSide;
+		document.querySelector('#closeSlider').onclick=closeSide;
+		document.querySelector('#calendarButton').onclick=changeView;
+		document.querySelector('#listButton').onclick=changeView;
 	}
-	else {
-		element_1.style.display = "none";
-		element_2.style.display = "block";
-	}
-}
 
-mainApp.config(['$httpProvider', function($httpProvider) {
-	$httpProvider.defaults.useXDomain = true;
-	delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}]);
+	// Controls the sliding out summary box
+	function openSide() {
+	    document.getElementById("slideout").style.width = "400px";
+	}
+
+	function closeSide() {
+	    document.getElementById("slideout").style.width = "0";
+	}
+
+	// Changes if you're looking at the shopping list or the recipes
+	function changeView() {
+		console.log("Hello");
+		var element_1 = document.getElementById("recipeSchedule");
+		var element_2 = document.getElementById("shoppingList");
+
+		if (element_1.style.display == "none"){
+			element_1.style.display = "block";
+			element_2.style.display = "none";
+		}
+		else {
+			element_1.style.display = "none";
+			element_2.style.display = "block";
+		}
+	}
+
+})();
